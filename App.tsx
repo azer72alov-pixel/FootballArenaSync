c96a2c95362b20210f22ab6785e2a780a0efc757
Az72er <azer72alov@gmail.com>
2026-01-09 02:57:50 +0400

    Update

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { COURTS } from './constants';
+import { TRANSLATIONS } from './translations';
+import Layout from './components/Layout';
+
+const App: React.FC = () => {
+  const [language, setLanguage] = useState<'en' | 'ru'>('en');
+
+  useEffect(() => {
+    const tg = (window as any).Telegram?.WebApp;
+    if (tg) {
+      tg.ready();
+      tg.expand();
+    }
+  }, []);
+
+  return (
+    <Layout language={language} setLanguage={setLanguage} courts={COURTS} translations={TRANSLATIONS} />
+  );
+};
+
+export default App;
