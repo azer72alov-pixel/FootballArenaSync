7fd2609aab01bb5edb787141df4bf0d51c91786e
Az72er <azer72alov@gmail.com>
2026-01-08 15:15:45 +0400

    Add comment about base path for Vercel
    Added a comment in the vite.config.ts file regarding the base path for Vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/',         // важно для путей на Vercel
   build: {
     outDir: 'dist',
     emptyOutDir: true
