c4e87d2819ac986badfc3b61c9b43532b16a7040
Az72er <azer72alov@gmail.com>
2026-01-10 13:47:16 +0400

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // Fix: Property 'cwd' does not exist on type 'Process' by casting process to any
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
+      // Это предотвращает ошибку "process is not defined" в браузере
+      'process.env': env,
       'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
       'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
     }
